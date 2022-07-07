3fb75aef1cfb52fbe41cf92fdca6dcf70037ced4
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2022-07-07 22:29:29 +0200

    init

---
+++ app/config/config.php
@@ -0,0 +1,19 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'ART');
+
+// App Root
+//echo dirname(dirname(__FILE__));
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'http://localhost:81/mvc/public/');
+// Site Name
+define('SITENAME', 'Hussein Hashad Art');
+
+define('APP_VERSION', '1.2');
+
+//public pages path
+define('VIEWS_PATH', '../app/views/');
